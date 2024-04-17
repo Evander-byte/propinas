813315ff81390f10c8af2945fa2bd63c7cb68e97
Evander-byte <emondragon571@gmail.com>
2024-04-17 14:21:16 -0600

    Funcionalidad completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 function App() {
 
-  const { addItem } = useOrder()
+  const { order, addItem, removeItem, tip, setTip } = useOrder()
 
   return (
     <>
@@ -21,12 +24,23 @@ function App() {
                 key={item.id}
                 item={item}
                 addItem={addItem}
+
               />
             ))}
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <h2>Consumo</h2>
+          <OrderContents
+            order={order}
+            removeItem={removeItem}
+          />
+          <TipPercentageForm
+            setTip={setTip}
+          />
+          <OrderTotals
+            order={order}
+            tip={tip}
+          />
         </div>
       </main>
     </>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,6 +4,7 @@ import type { MenuItem, OrderItem } from "../types";
 export default function useOrder() {
 
     const [order, setOrder] = useState<OrderItem[]>([])
+    const [tip, setTip] = useState(0);
 
     const addItem = (item: MenuItem) => {
         const itemExist = order.find(orderItem => orderItem.id === item.id)
@@ -16,8 +17,15 @@ export default function useOrder() {
             setOrder([...order, newItem])
         }
     }
-    console.log(order)
+
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(order.filter(item => item.id !== id))
+    }
     return {
-        addItem
+        order,
+        tip,
+        setTip,
+        addItem,
+        removeItem
     }
 }
